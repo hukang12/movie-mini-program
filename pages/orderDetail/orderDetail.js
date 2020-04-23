613eba54ff0ec93b9c8c605f219eb0c14b495ede
hukang12 <1603356819@qq.com>
2020-04-24 07:49:40 +0800

    0424commit

--- pages/orderDetail/orderDetail.js
+++ pages/orderDetail/orderDetail.js
@@ -7,6 +7,7 @@ Page({
    */
   data: {
     order:'',
+    oId:'',
     paytime:'',
     status:'yiwancheng'
   },
@@ -17,22 +18,23 @@ Page({
   onLoad: function (options) {
     var that = this;
     var orderId = this.options.orderId;
-    console.log("detail:"+orderId);
+    console.log("detail:" + orderId);
     wx.request({
       url: 'http://192.168.5.153:8088/movie/orders/TheOrder',
-      data:{
-        orderId:orderId
+      data: {
+        orderId: orderId
       },
-      method:'GET',
-      success:function(res){
+      method: 'GET',
+      success: function (res) {
         var order = res.data.order;
-        if(order!=null){
+        if (order != null) {
           var time = utils.formatTime(new Date(order.playTime));
           that.setData({
-            order:order,
-            paytime:time
+            order: order,
+            paytime: time,
+            oId: orderId
           })
-        }else{
+        } else {
           console.log("获取数据失败！")
         }
         //console.log(res.data);
@@ -40,7 +42,7 @@ Page({
 
     })
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -52,7 +54,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+   
   },
 
   /**
@@ -88,5 +90,36 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  deleteOrder: function () {
+      var id = this.data.oId;
+      wx.request({
+        url: 'http://192.168.5.153:8088/movie/orders/delete',
+        method: 'GET',
+        data:{
+          orderId:id
+        },
+        success:function(res){
+          if(res.data.success){
+            wx.showToast({
+              title: '删除成功！',
+              icon:'',
+              duration:2000
+            });
+            var page = getCurrentPages();
+            //获取上一个页面的页面栈
+            var lastPage = page[page.length - 2];
+            lastPage.onLoad();
+            wx.navigateBack({
+              delta:1
+            })
+          }else{
+            wx.showToast({
+              title: '系统异常',
+              icon: 'none',
+            })
+          }
+        }
+      })
+  },
 })
